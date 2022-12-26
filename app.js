151279c7d6b9d9bee291229c4329c733064f3ed9
vasilis <vasilispap@gmail.com>
2022-12-26 18:41:03 +0200

    basic setup

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
-
 const app=express()
 
+const path = require('path')
+
+app.use(express.static('public'))
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
 app.get('/', function(req,res){
-    res.send("Welcome to our new app")
+    console.log(app.get('views'))
+    res.render('home-guest')
 })
 
 app.listen(3000)
